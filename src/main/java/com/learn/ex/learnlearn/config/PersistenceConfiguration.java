36b7ef65a4558f3415d812cdc6f16264f8b0ad0c
vnikitchyn <Vnikitchyn@gmail.com>
2020-04-17 14:00:09 +0300

    changed to use DB_URL

--- src/main/java/com/learn/ex/learnlearn/config/PersistenceConfiguration.java
+++ src/main/java/com/learn/ex/learnlearn/config/PersistenceConfiguration.java
@@ -8,13 +8,13 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource() {
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/postgres");
-        builder.username("postgres");
-        builder.password("Welcome");
-        System.out.println("*** my custom datasource bean initialised ***");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource() {
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/postgres");
+//        builder.username("postgres");
+//        builder.password("Welcome");
+//        System.out.println("*** my custom datasource bean initialised ***");
+//        return builder.build();
+//    }
 }
